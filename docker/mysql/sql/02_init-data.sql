69acaafb8c84216b5ddf99260d8e6a853a870ba2
pokuwagata <pokuwagata@gmail.com>
2019-07-21 17:04:14 +0900

    config mysql

---
+++ docker/mysql/sql/02_init-data.sql
@@ -0,0 +1,20 @@
+INSERT INTO `sample_db`.`article_statuses`
+(`status`)
+VALUES
+("公開");
+
+INSERT INTO `sample_db`.`article_statuses`
+(`status`)
+VALUES
+('削除');
+
+
+INSERT INTO `sample_db`.`user_statuses`
+(`status`)
+VALUES
+('有効');
+
+INSERT INTO `sample_db`.`user_statuses`
+(`status`)
+VALUES
+('無効');
